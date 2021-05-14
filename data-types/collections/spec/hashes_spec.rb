5bb30c9e6517fda29f4d50b2d8708255fa32c82e
Jason Knoll <jason.p.knoll@gmail.com>
2021-05-14 12:08:21 -0700

    Complete unicorn

--- data-types/collections/spec/hashes_spec.rb
+++ data-types/collections/spec/hashes_spec.rb
@@ -1,21 +1,21 @@
 RSpec.describe 'Hashes' do
   it 'test 1' do
     # In the line below, create a new empty hash called empty
-    empty = ______
+    empty = {}
     expect(empty).to eq({})
   end
 
-  xit 'test 2' do
+  it 'test 2' do
     # In the line below, create an empty hash with a default value 0
-    empty = ______
+    empty = { :not_found => 0 }
     expect(empty[:not_found]).to eq(0)
   end
 
-  xit 'test 3' do
+  it 'test 3' do
     # In the line below, create a hash called ages. The hash should
     # have a key of "ben" with a value of 4 and a key of "kelly" with
     # a value of 6
-    ages = ______
+    ages = {"ben" => 4, "kelly" => 6}
     expect(ages.length).to eq(2)
     expect(ages["ben"]).to eq(4)
     expect(ages["kelly"]).to eq(6)
@@ -25,14 +25,14 @@ RSpec.describe 'Hashes' do
     # In the line below, create a hash called ages. The hash should
     # have a key of :ben with a value of 4 and a key of :kelly with
     # a value of 6.
-    ages = ______
+    ages = ____
     expect(ages.length).to eq(2)
     expect(ages["ben"]).to eq(4)
     expect(ages["kelly"]).to eq(6)
 
            # There are two different syntaxes for doing this. Use the other way to
            # achieve the same result
-           ages = ______
+           ages = _____
            assert_equal 2, ages.length
            assert_equal 4, ages[:ben]
            assert_equal 6, ages[:kelly]
@@ -248,4 +248,3 @@ RSpec.describe 'Hashes' do
     expect(opposite).to eq(expected)
   end
 end
-
