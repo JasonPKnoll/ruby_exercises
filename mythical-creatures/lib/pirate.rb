12e0db7a3183a573a1490d97a1a60c5765b94a04
Jason Knoll <jason.p.knoll@gmail.com>
2021-06-15 18:26:08 -0700

    Add pirate

---
+++ mythical-creatures/lib/pirate.rb
@@ -0,0 +1,23 @@
+class Pirate
+  attr_reader :name, :job, :curse, :booty
+
+  def initialize(name, job = 'Scallywag')
+    @name = name
+    @job = job
+    @curse = 0
+    @booty = 0
+  end
+
+  def commit_heinous_act
+    @curse += 1
+  end
+
+  def cursed?
+    curse >= 3
+  end
+
+  def rob_a_ship
+    @booty += 100
+  end
+
+end
